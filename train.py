ccbb9a6ffe8b0935a78dc4b19e6bb660b42edd59
Anmol-Maheshwari30 <86555725+Anmol-Maheshwari30@users.noreply.github.com>
2025-12-17 23:42:06 +0530

    Update train.py

--- train.py
+++ train.py
@@ -5,23 +5,29 @@ from model import yolov1
 from loss import yolo_loss
 
 voc_root = r"data\VOCtrainval_06-Nov-2007\VOCdevkit"
+ids = load_image_ids(voc_root)[:100]
 
-ids = load_image_ids(voc_root)[:5]
+def voc_generator (image_ids, voc_root, batch_size = 16):
+  while True:
+    np.random.shuffle(image_ids)
+    for i in range(0, len(image_ids) , batch_size):
+      batch_ids = image_ids[i:i+batch_size]
+      x_batch,y_batch = [],[]
+      for img_id in ids:
+        img = load_image(voc_root, img_id)
+        if img is None:
+          continue
+        boxes, labels = parse_annotation(voc_root, img_id)
+        target = encode_yolo_target(boxes, labels)
+        x_batch.append(img)
+        y_batch.append(target)
 
-x, y =[],[]
-for img_id in ids:
-    img = load_image(voc_root, img_id)
-    if img is None:
-        continue
-    boxes, labels = parse_annotation(voc_root, img_id)
-    target = encode_yolo_target(boxes, labels)
-    x.append(img)
-    y.append(target)
+        if len(x_batch) > 0:
+           yield np.array(x_batch, dtype=np.float32), np.array(y_batch, dtype=np.float32)
 
-x = np.array(x , dtype = np.float32)
-y = np.array(y , dtype = np.float32)
+all_ids = load_image_ids(voc_root)
+train_gen = voc_generator(all_ids, voc_root, batch_size = 16)
 
 model = yolov1()
-model.compile(optimizer = "adam" , loss = yolo_loss)
-model.fit(x,y,epochs = 5)
-
+model.compile(optimizer =tf.keras.optimizers.Adam(learning_rate = 1e-4), loss = yolo_loss)
+model.fit(train_gen, steps_per_epoch = len(all_ids)//16,epochs = 5)
